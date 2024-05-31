cbd261b12fc8b6e3aab1d4fd49b6f2141292051a
DanDi code <dan--di@mail.ru>
2024-05-31 11:07:04 +0300

    31.05.24

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from .views import index, home
-from .views import MyLoginView
+from .views import MyLoginView, RegisterView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path("",home , name="home"),
     path("start/", index, name="index"),
     path("login/", MyLoginView.as_view(), name="login"),
+    path('logout/', LogoutView.as_view(next_page="login"), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
 ]
--- users/views.py
+++ users/views.py
@@ -4,11 +4,15 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
+from django.views.generic.edit import FormView
 from django.contrib import messages
+from .forms import RegisterForm
+from django.contrib.auth import login
 
+menu = []
 
 def home(request):
-    return render(request, "home.html")
+    return render(request, "home.html", {'menu': menu, 'title': 'Главная страница'})
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
@@ -18,8 +22,20 @@ class MyLoginView(LoginView):
     template_name = "users/login.html"
 
     def get_success_url(self):
-        return reverse_lazy('index')
+        return reverse_lazy('home')
     
     def form_invalid(self, form):
         messages.error(self.request, 'invalid username or password')
         return self.render_to_response(self.get_context_data(form=form))
+    
+class RegisterView(FormView):
+    template_name = 'users/register.html'
+    form_class = RegisterForm
+    redirect_authenticated_user = True
+    success_url = reverse_lazy('home')
+
+    def form_valid(self, form):
+        user = form.save()
+        if user:
+            login(self.request, user)
+        return super(RegisterView, self).form_valid(form)
